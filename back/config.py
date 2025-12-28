bb6fd96d7345e5349dec1a287a8fdb13400de0ff
Matt Griff <matthieu.griffonnet1@gmail.com>
2025-12-28 17:15:26 +0100

    Update Back

--- back/config.py
+++ back/config.py
@@ -2,3 +2,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI =  'sqlite:///master.db'
     SQLALCHEMY_BINDS = {'replica': 'sqlite:///replica.db'}
     JWT_SECRET_KEY = 'xin-jojo-dome-island-party'  
+    JWT_TOKEN_LOCATION = ["cookies"]
+    JWT_COOKIE_SECURE = False
+    JWT_COOKIE_SAMESITE = "Lax"
+    JWT_COOKIE_CSRF_PROTECT = False
--- back/routes/users.py
+++ back/routes/users.py
@@ -1,6 +1,6 @@
 # routes/users.py
 from flask import Blueprint, request, jsonify
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, set_access_cookies
 from services.user_service import UserService
 import datetime
 
@@ -14,7 +14,9 @@ def register():
     try:
         user = UserService.register(data.get("username"), data.get("password"))
         access_token = create_access_token(identity=str(user.id), expires_delta=datetime.timedelta(seconds=delta_time))
-        return jsonify(access_token=access_token), 201
+        response = jsonify(access_token=access_token)
+        set_access_cookies(response, access_token)
+        return response, 201
     except ValueError as e:
         return jsonify({"error": str(e)}), 400
 
@@ -25,7 +27,9 @@ def login():
     if not user:
         return jsonify({"error": "invalid credentials"}), 401
     access_token = create_access_token(identity=str(user.id), expires_delta=datetime.timedelta(seconds=delta_time))
-    return jsonify(access_token=access_token), 200
+    response = jsonify(access_token=access_token)
+    set_access_cookies(response, access_token)
+    return response, 200
 
 @users_bp.route('/protected', methods=['GET'])
 @jwt_required()
@@ -33,3 +37,10 @@ def protected():
     current_user_id = get_jwt_identity()
     user = UserService.get_user(current_user_id)
     return jsonify(logged_in_as=user.nom), 200
+
+@users_bp.route('/logout', methods=['POST'])
+@jwt_required()
+def logout():
+    response = jsonify(msg="logout successful")
+    response.delete_cookie('access_token_cookie')
+    return response, 200
