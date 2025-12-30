59d20564c521442af4b87f1e6883aee715bf6e10
al-najjar gabriel <gabriel.al-najjar@etu.univ-cotedazur.fr>
2025-12-30 19:49:10 +0100

    ajout de la route

--- front-app/src/App.js
+++ front-app/src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
 import { Register } from "./pages/Register/Register";
 import { Login } from "./pages/Login/Login";
-
+import { NewNote } from "./components/notes/create/NewNote";
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/notes/new" element={<NewNote />} />
       <Route path="*" element={<Home />} />
     </Routes>
   );
